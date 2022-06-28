2bded7e424bdf32683e903719f483d3cbb1ffe97
adriano328 <lucasadrianodias@gmail.com>
2022-06-28 14:09:01 -0400

    Envio de estrutura spring

---
+++ src/main/java/com/totem/onboarding/onboardingback/OnboardingBackApplication.java
@@ -0,0 +1,13 @@
+package com.totem.onboarding.onboardingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totem/onboarding/onboardingback/OnboardingBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totem.onboarding.onboardingback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
